51b22377c03a34bec88debe87242ef58196de762
erwincarlogonzales <erwincarlogonzales@gmail.com>
2023-10-16 19:18:42 +0800

    updated variables, visualizations, readme and dependencies
    updated model names, created visualizations using PCA with the 2 most important features. Updated readme and dependencies.txt files

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # iris_app
  simple streamlit app for the iris and wine dataset
 
-a quick introduction to data science app deployment using streamlit
-
 to run the app type: streamlit run main.py
+
+Iris Prediction Model
+
+This is my Iris/Wine Prediction Model Streamlit application. A quick introduction to data science app deployment using streamlit
+
+Features:
+- Interactive User Interface: Seamlessly choose between datasets and classifiers.
+- Dynamic Visualizations: Witness the power of dimensionality reduction with PCA and understand data distributions with histograms.
+- Real-time Predictions: Get predictions with different classifiers and instantly view the accuracy.
+
+Getting Started:
+1. Installation: Ensure you have all the required dependencies installed. Use the provided dependencies.txt file with the command:
+pip install -r dependencies.txt
+2. Running the App: Navigate to the project directory and run:
+streamlit run main.py
+3. Choose & Explore: Use the sidebar to select datasets and classifiers, adjust model parameters, and let the magic happen!
+
+Contribute:
+This project welcomes contributions. Fork the repository, make your changes, and submit a pull request.
+
+Feedback & Issues:
+For feedback, suggestions, or issues, please contact the developer.
+
+License: MIT
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import dependencies
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit as st
 
 # import datasets
@@ -12,6 +13,7 @@ from sklearn.model_selection import train_test_split
 # models
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
 
 # metrics
 from sklearn.metrics import accuracy_score
@@ -29,7 +31,7 @@ st.write("""
 dataset = st.sidebar.selectbox('Select Dataset', ('Iris', 'Wine'))
 
 # select a built-in ML model as a sidebar selectbox
-model = st.sidebar.selectbox('Select Classifier', ('KNN', 'Random Forest'))
+classifier_name = st.sidebar.selectbox('Select Classifier', ('KNN', 'Random Forest'))
 
 # create a fuction to get dataset from sklearn
 def get_dataset(dataset):
@@ -53,13 +55,13 @@ st.write(f'the dataset contains {len(X)} columns and {X.shape[1]} rows')
 st.write('Number of classes', len(np.unique(y)))
 
 # create a function that selects the parameters for the ML models using a slider
-def add_param_ui(classifier_name):
+def add_param_ui(model_name):
     
     # create an empty dictionary
     params = dict()
     
     # create a slider for the parameters
-    if classifier_name == 'KNN':
+    if model_name == 'KNN':
         K = st.sidebar.slider('K-neighbors', 1, 10)
         
         # append the dictionary
@@ -77,36 +79,74 @@ def add_param_ui(classifier_name):
     return params
 
 # call the function with the model
-params = add_param_ui(model)
+params = add_param_ui(classifier_name)
 
 # create a function that calls the ML classifier
-def get_classifier(classifier_name, params):
+def get_classifier(model_name, params):
     
     # create a slider for the parameters
-    if classifier_name == 'KNN':
-        classifier = KNeighborsClassifier(n_neighbors=params['K'])   
+    if model_name == 'KNN':
+        model = KNeighborsClassifier(n_neighbors=params['K'])   
     else:
         # random forest parameters
-        classifier = RandomForestClassifier(n_estimators = params['n_estimators'],
+        model = RandomForestClassifier(n_estimators = params['n_estimators'],
                                             max_depth = params['max_depth'], random_state = 42)
     
-    return classifier
+    return model
 
 # call the function
-classifier = get_classifier(model, params)
+model = get_classifier(classifier_name, params)
         
 # split the dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
 
 # fit the dataset
-classifier.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
 # make prediction
-y_pred = classifier.predict(X_test)
+y_pred = model.predict(X_test)
 
 # evaluate model accuracy
 acc = accuracy_score(y_test, y_pred)
 
 # display accuracy
-st.write(f'Classifier {model}')
+st.write(f'Classifier {classifier_name}')
 st.write(f'Accuracy {acc*100:.2f}%')
+
+# create visualization
+# PCA for dimensionality reduction
+pca = PCA(2)
+X_projected = pca.fit_transform(X)
+
+# Get the feature names
+feature_names = datasets.load_iris().feature_names if dataset == 'Iris' else datasets.load_wine().feature_names
+
+# Identify the most influential feature for each principal component
+most_influential_feature_x1 = feature_names[np.argmax(np.abs(pca.components_[0]))]
+most_influential_feature_x2 = feature_names[np.argmax(np.abs(pca.components_[1]))]
+
+# Displaying the most influential features for x1 and x2
+st.write(f'Most influential feature for PCA Feature 1: {most_influential_feature_x1}')
+st.write(f'Most influential feature for PCA Feature 2: {most_influential_feature_x2}')
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+# Scatter plot for PCA features x1 and x2 using Seaborn
+fig, ax = plt.subplots()
+sns.scatterplot(x=x1, y=x2, hue=y, palette='viridis', ax=ax)
+ax.set_xlabel(f'PCA Feature 1 ({most_influential_feature_x1})')
+ax.set_ylabel(f'PCA Feature 2 ({most_influential_feature_x2})')
+st.pyplot(fig)
+
+# Histogram for PCA feature x1 using Seaborn
+fig, ax = plt.subplots()
+sns.histplot(x1, kde=True, ax=ax)
+ax.set_title(f'PCA Feature 1 ({most_influential_feature_x1}) Histogram')
+st.pyplot(fig)
+
+# Histogram for PCA feature x2 using Seaborn
+fig, ax = plt.subplots()
+sns.histplot(x2, kde=True, ax=ax)
+ax.set_title(f'PCA Feature 2 ({most_influential_feature_x2}) Histogram')
+st.pyplot(fig)
